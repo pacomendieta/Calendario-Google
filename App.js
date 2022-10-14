89dc54251871f2e9e9777a4bec72eaea79a1aef2
pacomendieta <ff.mendieta@gmail.com>
2022-10-14 14:38:12 +0200

    Google ha cambiado los apis y no van

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const express = require( 'express' );
 var app=express();
 const  passport = require("passport");
 
-const GoogleStrategy = require( "passport-google-oauth20" );
+const GoogleStrategy = require( "passport-google-oauth20" ).Strategy;
 const googleapi = require ("./googleapikey.js"); //clave de api y client id de Google
 
 app.get("/", (request,response)=>{
@@ -13,19 +13,18 @@ app.listen(process.env.puerto);
 app.set('view engine', 'ejs');
 //console.log("Escuchando en puerto: ", process.env);
 
-app.post("/login", (request,response)=>{
-    response.send("Pagina de Login");
-})
+console.log("GOOGLE API:", googleapi)
 
 // IDENTIFICACION CON CUENTA DE GOOGLE CON PASSPORT
-
+// Configurar Estrategia de Autenticación => Autenticacion con clave Oauth en Google
 passport.use ( new GoogleStrategy(
     {
         clientID:googleapi.client_id ,
         clientSecret: googleapi.client_secret,
-        callbackURL: googleapi.redirect_uris ,
+        callbackURL: googleapi.redirect_uris[0] ,
 
     }, function (accessToken, refreshToken, profile, cb) {
+        // Guardar datos de usuario recibidos en Sesion, BD, etc...
         var user = {
             accessToken: accessToken,
             refreshToken: refreshToken,
@@ -37,10 +36,30 @@ passport.use ( new GoogleStrategy(
 //password llama a esta funcion cuando se completa un login de usuario
 // se almacena el usuario en sesion
 passport.serializeUser( function(user,done) {
-    done
+    // user = var user  definido en la funcion pasada a passport.use
+    done(null,user);
 })
 //password llama a esta funcion para localizar datos usuario almacenados 
 // en la sesion
 passport.deserializeUser( function(user,done) {
-    done
+    done(null,user); //accion final obligada. null=no hay error.
+})
+// PAGINA DE LOGIN - Proceso de Autenticacion
+app.post("/login", passport.authenticate('google', {
+    // scope=permisos solicitados en Google:
+    scope: [
+         'https://www.googleapis.com/auth/userinfo.profile',  // Google +  ??
+         'https://www.googleapis.com/auth/calendar',  //Google calendar
+         'https://www.googleapis.com/auth/userinfo.email' // ???
+   ]
+} ));
+//PAGINA DE CALLBACK
+app.get("/auth/google/callback",
+    passport.authenticate('google',{ failureRedirect:"/auth/error"}),
+    (request,response)=>{  response.send(request.session) }
+)
+
+//Pagina de error de autenticacion
+app.get("/auth/error", (request,response)=>{
+    response.send("ERROR EN LA AUTENTICACION !!")
 })
