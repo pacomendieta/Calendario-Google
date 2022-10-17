4bf8449d50cdcaef4870596b0556cd4e62c8d845
pacomendieta <ff.mendieta@gmail.com>
2022-10-17 13:32:48 +0200

    OAuth en Google ok

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-const express = require( 'express' );
-var app=express();
-const  passport = require("passport");
 
-const GoogleStrategy = require( "passport-google-oauth20" ).Strategy;
-const googleapi = require ("./googleapikey.js"); //clave de api y client id de Google
+
+const express= require('express');
+const passport= require('passport')
+const path= require('path')
+const googleapi = require('./googleapikey') //clave de api y client id de Google
+const GoogleStrategy= require('passport-google-oauth20').Strategy
+
+
+var app=express();
 
 // Express-Session 
 const session = require('express-session');
@@ -13,54 +17,69 @@ app.use(session( {
   resave: false
 }))
 
+// DATOS DE LA APP REGISTRADOS EN GOOGLE DEVELOPPER CONSOLE
+CLIENT_ID=googleapi.client_id
+CLIENT_SECRET=googleapi.client_secret
+CALLBACK_URL=googleapi.redirect_uris[0]
+
+
+
+
 // configuring passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.get("/", (request,response)=>{
-    response.render("index");
+//passPORT llama a esta funcion tras login de usuario
+passport.serializeUser( function(id,done) {
+    // user = var user  definido en la funcion pasada a passport.use
+    done(null,id);
+}) 
+//passport llama a esta funcion para localizar datos usuario almacenados en la sesion
+passport.deserializeUser( function(id,done) {
+    done(null,id); //accion final obligada. null=no hay error.
 })
 
-app.listen(process.env.puerto);
-app.set('view engine', 'ejs');
-//console.log("Escuchando en puerto: ", process.env);
+//console.log("GOOGLE API:", googleapi)
 
-console.log("GOOGLE API:", googleapi)
-
-// IDENTIFICACION CON CUENTA DE GOOGLE CON PASSPORT
-// Configurar Estrategia de Autenticación => Autenticacion con clave Oauth en Google
+// IDENTIFICACION CON CUENTA DE GOOGLE CON PASSPORT  Configurar Estrategia de Autenticación => Autenticacion con clave Oauth en Google
 passport.use ( new GoogleStrategy(
     {
-        clientID:googleapi.client_id ,
-        clientSecret: googleapi.client_secret,
-        callbackURL: googleapi.redirect_uris[0] ,
-
-    }, async function (accessToken, refreshToken, profile, email,cb) {
+        clientID: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        callbackURL: CALLBACK_URL
+    
+    }, 
+    
+    async function (accessToken, refreshToken, profile, email,cb) {
         // Guardar datos de usuario recibidos en Sesion, BD, etc...
-        //var user = {
-        //    accessToken: accessToken,
-        //    refreshToken: refreshToken,
-        //    profile: profile
-        //};
+        var user = {
+            accessToken: accessToken,
+            refreshToken: refreshToken,
+            profile: profile
+        };
         return cb(null, email.id);
     }
 ) );
-//password llama a esta funcion cuando se completa un login de usuario
-// se almacena el usuario en sesion
-passport.serializeUser( function(user,done) {
-    // user = var user  definido en la funcion pasada a passport.use
-    done(null,user);
+
+// HOME PAGE
+app.get("/", (req,res)=>{
+   res.render("index");
+   //res.sendFile(path.join(__dirname + '/public/index.html'))
 })
-//password llama a esta funcion para localizar datos usuario almacenados 
-// en la sesion
-passport.deserializeUser( function(user,done) {
-    done(null,user); //accion final obligada. null=no hay error.
+
+
+
+
+// PAGINA SUCCESS - LOGIN OK
+app.get('/success', (req, res)=>
+{
+    res.sendFile(path.join(__dirname + '/public/loginok.html'))
 })
+
 // PAGINA DE LOGIN - Proceso de Autenticacion
 app.get('/google/auth',
   passport.authenticate('google', {scope: ['profile', 'email']})
 )
-
 
 //PAGINA DE CALLBACK
 app.get('/auth/google/callback',
@@ -71,13 +90,14 @@ app.get('/auth/google/callback',
   }
 )
 
-// PAGINA SUCCESS - LOGIN OK
-app.get('/success', (req, res)=>
-{
-  res.send("LOGIN OK. PAGINA SUCCESS")
-})
+// running server
+app.listen(process.env.puerto, ()=>{
+    console.log("Server is running on Port " + process.env.puerto)  
+});
+app.set('view engine', 'ejs');
+
 
 //Pagina de error de autenticacion
-app.get("/auth/error", (request,response)=>{
-    response.send("ERROR EN LA AUTENTICACION !!")
-})
+//app.get("/auth/error", (request,response)=>{
+//    response.send("ERROR EN LA AUTENTICACION !!")
+//})
